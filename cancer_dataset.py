e1a914e9d0eea2242776a52ca7a61fc59ef71a3e
Iain Lee <iclee141@m8int01.fsl.byu.edu>
2018-09-26 11:52:06 -0600

    first

---
+++ cancer_dataset.py
@@ -0,0 +1,42 @@
+import torchvision
+import os
+import gzip
+import tarfile
+import gc
+from IPython.core.ultratb import AutoFormattedTB
+__ITB__ = AutoFormattedTB(mode = 'Verbose',color_scheme='LightBg', tb_offset = 1)
+
+class CancerDataset(Dataset):
+  def __init__(self, root, download=True, size=512, train=True):
+    if download and not os.path.exists(os.path.join(root, 'cancer_data')):
+      datasets.utils.download_url('http://liftothers.org/cancer_data.tar.gz', root, 'cancer_data.tar.gz', None)
+      self.extract_gzip(os.path.join(root, 'cancer_data.tar.gz'))
+      self.extract_tar(os.path.join(root, 'cancer_data.tar'))
+
+    postfix = 'train' if train else 'test'
+    root = os.path.join(root, 'cancer_data', 'cancer_data')
+    self.dataset_folder = torchvision.datasets.ImageFolder(os.path.join(root, 'inputs_' + postfix) ,transform = transforms.Compose([transforms.Resize(size),transforms.ToTensor()]))
+    self.label_folder = torchvision.datasets.ImageFolder(os.path.join(root, 'outputs_' + postfix) ,transform = transforms.Compose([transforms.Resize(size),transforms.ToTensor()]))
+
+  @staticmethod
+  def extract_gzip(gzip_path, remove_finished=False):
+    print('Extracting {}'.format(gzip_path))
+    with open(gzip_path.replace('.gz', ''), 'wb') as out_f, gzip.GzipFile(gzip_path) as zip_f:
+      out_f.write(zip_f.read())
+    if remove_finished:
+      os.unlink(gzip_path)
+
+  @staticmethod
+  def extract_tar(tar_path):
+    print('Untarring {}'.format(tar_path))
+    z = tarfile.TarFile(tar_path)
+    z.extractall(tar_path.replace('.tar', ''))
+
+
+  def __getitem__(self,index):
+    img = self.dataset_folder[index]
+    label = self.label_folder[index]
+    return img[0],label[0][0]
+
+  def __len__(self):
+    return len(self.dataset_folder)
---
+++ cancer_model.py
@@ -0,0 +1,109 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import transforms, utils, datasets
+from torch.nn.parameter import Parameter
+from tqdm import tqdm
+import pdb
+
+# assert torch.cuda.is_available() # You need to request a GPU from Runtime > Change Runtime Type
+
+from cancer_dataset import CancerDataset
+
+
+class UNet(nn.Module):
+
+  def __init__(self, input_size):
+    super(UNet, self).__init__()
+
+    self.layer1 = nn.Sequential(
+      nn.Conv2d(input_size[0], 64, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+      nn.ReLU()
+    )
+
+    self.layer2 = nn.Sequential(
+      nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+      nn.ReLU()
+    )
+
+    self.layer3 = nn.Sequential(
+      nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+      nn.ReLU()
+    )
+
+    self.layer4 = nn.Sequential(
+      nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+      nn.ReLU()
+    )
+
+    self.midlayer = nn.Sequential(
+      nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1)
+      nn.ReLU(),
+      nn.ConvTranspose2d(1024, 512, kernel_size=3, stride=1, padding=0)
+    )
+
+    self.uplayer4 = nn.Sequential(
+      nn.Conv2d(1024, 512, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+      nn.ReLU(),
+      nn.ConvTranspose2d(512, 256, kernel_size=3, stride=1, padding=0)
+    )
+
+    self.uplayer3 = nn.Sequential(
+      nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+      nn.ReLU(),
+      nn.ConvTranspose2d(256, 128, kernel_size=3, stride=1, padding=0)
+    )
+
+    self.uplayer2 = nn.Sequential(
+      nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+      nn.ReLU(),
+      nn.ConvTranspose2d(128, 64, kernel_size=3, stride=1, padding=0)
+    )
+
+    self.uplayer1 = nn.Sequential(
+      nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+      nn.ReLU(),
+      nn.Conv2d(64, 2, kernel_size=3, stride=1, padding=1)
+    )
+
+    self.maxpool = nn.MaxPool2d(kernel_size=2)
+
+
+
+
+  def forward(self, x):
+    x1 = self.layer1( x )
+    x2 = self.layer2( self.maxpool(x1) )
+    x3 = self.layer3( self.maxpool(x2) )
+    x4 = self.layer4( self.maxpool(x3) )
+
+    xout = self.midlayer( self.maxpool(x4) )
+
+    xout = self.uplayer4( torch.cat(xout, x4) )
+    xout = self.uplayer3( torch.cat(xout, x3) )
+    xout = self.uplayer2( torch.cat(xout, x2) )
+    xout = self.uplayer1( torch.cat(xout, x1) )
+
+    return xout
