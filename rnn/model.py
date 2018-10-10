6fbda61f1179da135e55fb90ffe08cc075d56b70
Iain Lee <icleen@gmail.com>
2018-10-10 13:22:42 -0600

    adding the new rnn project files

---
+++ rnn/model.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+
+
+class GRU(nn.Module):
+    """docstring for GRU."""
+    def __init__(self, input_size, hidden_size, num_layers=1, bias=True, batch_first=False, dropout=0, bidirectional=False):
+        super(GRU, self).__init__()
+        self.__dict__.update(locals())
+
+
+    def forward(self, inp, hidden):
+        # do something
+
+
+
+class GRUNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, n_layers=1):
+        super(GRUNet, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def forward(self, input_char, hidden):
+        output = self.embedding(input_char).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def init_hidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=self.device)
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, n_layers=1):
+        super(RNN, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+
+
+        # encode using embedding layer
+        # set up GRU passing in number of layers parameter (nn.GRU)
+        # decode output
+
+        # pytorch example rnn code
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.o2o = nn.Linear(hidden_size + output_size, output_size)
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input_char, hidden):
+        # by reviewing the documentation, construct a forward function that properly uses the output
+        # of the GRU
+        # return output and hidden
+
+        # pytorch example rnn code
+        input_combined = torch.cat((input_char, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i2o(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.o2o(output_combined)
+        output = self.dropout(output)
+        output = self.softmax(output)
+        return output, hidden
+
+    def init_hidden(self):
+        return torch.zeros(1, self.hidden_size)
---
+++ rnn/utils.py
@@ -0,0 +1,45 @@
+import torch
+import unidecode
+import string
+import random
+
+
+all_characters = string.printable
+n_characters = len(all_characters)
+
+file = unidecode.unidecode(open('./text_files/lotr.txt').read())
+file_len = len(file)
+print('file_len =', file_len)
+
+chunk_len = 200
+
+
+def random_chunk():
+    start_index = random.randint(0, file_len - chunk_len)
+    # start_index = 0
+    end_index = start_index + chunk_len + 1
+    return file[start_index:end_index]
+
+print(random_chunk())
+
+# Turn string into list of longs
+def char_tensor(string):
+    tensor = torch.zeros(len(string)).long()
+    for c in range(len(string)):
+        tensor[c] = all_characters.index(string[c])
+    return tensor
+
+print(char_tensor('abcDEF'))
+
+def onehot(tnsr):
+    out = torch.zeros((tnsr.size(0), n_characters))
+    for i in range(tnsr.size(0)):
+        out[i][tnsr[i]] = 1
+    return out
+
+def random_training_set():
+    chunk = random_chunk()
+    inp = char_tensor(chunk[:-1])
+    target = char_tensor(chunk[1:])
+    # inp = onehot(inp)
+    return inp, target
