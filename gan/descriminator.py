7259f0d22a6eb98ad3df2c0b90a31831dd1ed67f
Iain Lee <icleen@gmail.com>
2018-11-02 13:04:59 -0600

    defined generator and descriminator

--- gan/descriminator.py
+++ gan/descriminator.py
@@ -1,1 +1,44 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms, utils, datasets
+from torch.nn.parameter import Parameter
+
+import pdb
+import numpy as np
+
+class Descriminator(nn.Module):
+  """docstring for Descriminator."""
+  def __init__(self, in_channels=3, img_shape=64):
+    super(Descriminator, self).__init__()
+    self.in_channels = in_channels
+
+    self.layer = nn.Sequential(
+      nn.Conv2d(in_channels, 128, kernel_size=3, stride=2, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
+      nn.ReLU(),
+      nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1),
+    )
+
+    out_shape = int(img_shape / pow(2, 4))
+    self.linear_size = out_shape*out_shape*1024
+    self.fc = nn.Linear(self.linear_size, 2)
+
+  def forward(self, x):
+    x = self.layer(x)
+    x = x.view(-1, self.linear_size)
+    x = self.fc(x)
+    return x
+
+if __name__ == '__main__':
+  x = torch.zeros((3, 64, 64), dtype=torch.float)
+  x.normal_()
+  x.unsqueeze_(0)
+  print(x.size())
+
+  descriminator = Descriminator()
+  out = descriminator(x)
+  print(out.size())
--- gan/generator.py
+++ gan/generator.py
@@ -6,3 +6,55 @@ from torch.nn.parameter import Parameter
 
 import pdb
 import numpy as np
+
+class Generator(nn.Module):
+  """docstring for Generator."""
+  def __init__(self, z_size=100):
+    super(Generator, self).__init__()
+    self.z_size = z_size
+
+    self.init_size = 4
+    self.init_channels = 1024
+    self.fc = nn.Linear(z_size, self.init_channels*self.init_size*self.init_size)
+
+    channel0 = self.init_channels
+    channel1 = 512
+    channel2 = 256
+    channel3 = 128
+    channel4 = 3
+    self.layer = nn.Sequential(
+      nn.ConvTranspose2d(
+        channel0, channel1, kernel_size=3, stride=2, padding=1, output_padding=1),
+      nn.Conv2d(channel1, channel1, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+
+      nn.ConvTranspose2d(
+        channel1, channel2, kernel_size=3, stride=2, padding=1, output_padding=1),
+      nn.Conv2d(channel2, channel2, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+
+      nn.ConvTranspose2d(channel2, channel3, kernel_size=3, stride=2, padding=1, output_padding=1),
+      nn.Conv2d(channel3, channel3, kernel_size=3, stride=1, padding=1),
+      nn.ReLU(),
+
+      nn.ConvTranspose2d( channel3, channel4, kernel_size=3, stride=2, padding=1, output_padding=1),
+      nn.Conv2d(channel4, channel4, kernel_size=3, stride=1, padding=1),
+      nn.Sigmoid(),
+    )
+
+  def forward(self, x=None):
+    if x is None:
+      x = torch.zeros((self.z_size), dtype=torch.float)
+      x.normal_()
+      x.unsqueeze_(0) # become a pretend batch
+
+    x = self.fc(x)
+    x = x.view(-1, self.init_channels, self.init_size, self.init_size)
+    x = self.layer(x)
+    return x
+
+
+if __name__ == '__main__':
+  net = Generator()
+  out = net.forward()
+  print(out.size())
