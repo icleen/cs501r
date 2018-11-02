780b23808b85c0412f0a1c086baaf0711b93dfe5
Iain Lee <icleen@gmail.com>
2018-11-02 14:50:49 -0600

    setup the loss functions

--- gan/loss.py
+++ gan/loss.py
@@ -1,1 +1,25 @@
 import torch
+
+class DescriminatorLoss(nn.Module):
+  """docstring for DescriminatorLoss."""
+  def __init__(self, lam):
+    super(DescriminatorLoss, self).__init__()
+    self.lam = lam
+
+  def forward(self, predg, predt, predh, xhat):
+    loss = torch.autograd.grad(predh, xhat)
+    print(loss.size())
+    loss = torch.autograd.grad(predh, xhat).norm(p=2, dim=1)
+    loss = loss - 1
+    loss = self.lam*(loss.pow(2))
+    loss = predg - predt + loss
+    return loss.mean()
+
+
+class GeneratorLoss(nn.Module):
+  """docstring for GeneratorLoss."""
+  def __init__(self):
+    super(GeneratorLoss, self).__init__()
+
+  def forward(self, pred):
+    return -1 * pred.mean()
